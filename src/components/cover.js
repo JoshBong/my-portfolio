c675539ebcff0c14ef4f13c0a6bb5953713e3eea
Joshua Huang <jhuang@10-21-70-149.dynapool.wireless.nyu.edu>
2025-10-02 14:03:44 -0400

    the pages are messed up for some reason the width of the pages is tied to its positioning due to the transformation on the y axis, idk how to seperate them without breaking everything atm

--- src/components/cover.js
+++ src/components/cover.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import styles from './Cover.module.css';
+import styles from './cover.module.css';
 
 export default function Cover() {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push('/project');
-  };
-
   return (
-    // 2. Access the class name on the 'styles' object.
-    <div className={styles.bookCover} onClick={handleClick}>
-      {/* 3. Use the class name in the JSX */}
-      <div className={styles.bookTitle}>The Code Cookbook</div>
-      <div className={styles.bookAuthor}>By Joshua Huang</div>
-      <div className={styles.bookDescription}>
-        A personal portfolio of my projects and experiences.
+    <div className={`${styles.bookVariables} ${styles.bookContainer}`}>
+      <div className={styles.book}>
+      <div className={styles.bookCover}>
+          <img src="/temp.png" alt="The Code Cookbook" className={styles.coverImage} />
+        </div>
       </div>
     </div>
   );
